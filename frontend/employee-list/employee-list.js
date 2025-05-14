a8d57977f0374bd76ae18624c5a7e0d59282b7d9
fsneax <fsneax@gmail.com>
2025-05-14 10:37:30 +0800

    major changes on employee-list

---
+++ frontend/employee-list/employee-list.js
@@ -0,0 +1,24 @@
+function navigateTo(page) {
+    switch (page) {
+      case 'dashboard':
+        window.location.href = '../dashboard/dashboard.html';
+        break;
+      case 'employee-list':
+        window.location.href = '../employee-list/employee-list.html';
+        break;
+      case 'positions':
+        window.location.href = '../positions/positions.html';
+        break;
+      case 'attendance':
+        window.location.href = '../attendance/attendance.html';
+        break;
+    }
+  }
+  
+  function logout() {
+    // Clear session (if any)
+    // Redirect to login
+    window.location.href = '../index/index.html';
+  }
+
+  
